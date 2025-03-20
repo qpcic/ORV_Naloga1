b1ab6380964a6d5eb3927643afa2b9f8b597b3c1
Jan Kupčič <kupcic.jan@gmail.com>
2025-03-20 21:49:52 +0100

    first commit

---
+++ naloga1.py
@@ -0,0 +1,43 @@
+import cv2 as cv
+import numpy as np
+
+def zmanjsaj_sliko(slika, sirina, visina):
+    '''Pomanjša sliko na določeno velikost.'''
+    return cv.resize(slika, (sirina, visina))
+
+
+
+def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) -> list:
+    '''Obdelamo sliko in preštejemo število pikslov kože v vsaki škatli.'''
+
+
+def prestej_piksle_z_barvo_koze(slika, barva_koze) -> int:
+    '''Prestej število pikslov z barvo kože v škatli.'''
+    pass
+
+
+def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj) -> tuple:
+    '''Izračuna spodnje in zgornje meje barve kože na podlagi izbranega območja.'''
+
+
+def main():
+    pass
+if __name__ == '__main__':
+    main()
+
+if __name__ == '__main__':
+    #Pripravi kamero
+
+    #Zajami prvo sliko iz kamere
+
+    #Izračunamo barvo kože na prvi sliki
+
+    #Zajemaj slike iz kamere in jih obdeluj     
+    
+    #Označi območja (škatle), kjer se nahaja obraz (kako je prepuščeno vaši domišljiji)
+        #Vprašanje 1: Kako iz števila pikslov iz vsake škatle določiti celotno območje obraza (Floodfill)?
+        #Vprašanje 2: Kako prešteti število ljudi?
+
+        #Kako velikost prebirne škatle vpliva na hitrost algoritma in točnost detekcije? Poigrajte se s parametroma velikost_skatle
+        #in ne pozabite, da ni nujno da je škatla kvadratna.
+    pass
