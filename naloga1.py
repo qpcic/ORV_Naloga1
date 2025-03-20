2a8cb2aeb7e069b3a34f7b46e15a87423bf3654e
Jan Kupčič <kupcic.jan@gmail.com>
2025-03-20 23:33:09 +0100

    Code cleanup

--- naloga1.py
+++ naloga1.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 
 def zmanjsaj_sliko(slika, sirina, visina):
-    pass
+
     return cv.resize(slika, (sirina, visina))
 
 def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) -> list:
